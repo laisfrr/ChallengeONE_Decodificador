5ba6c4f9d2e9c6bf245d030c57866aa7f037c627
laisfrr <laisf.oliv@gmail.com>
2023-04-27 20:56:51 -0300

    feat: update responsividade, adicionado script e funcionalidades

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,58 @@
-var texto = document.querySelector('.saida-texto')
-document.write('texto')
+var entradaTexto = document.querySelector('.entrada-textarea')
+var textoRecebido = entradaTexto.value
+
+var criptografiaBotao = document.querySelector('.button-cripto')
+
+function criptografar() {
+  var textoRecebido = entradaTexto.value.toLowerCase()
+  textoCripto = textoRecebido
+    .replace(/a/g, 'ai')
+    .replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat')
+  var img = document.querySelector('.img-container')
+  var copy = document.querySelector('.button-copy')
+  var texto = document.querySelector('.container-texto')
+  document.querySelector('.container-texto').innerHTML = textoCripto
+  img.style.visibility = 'hidden'
+  copy.style.visibility = 'inherit'
+  texto.style.display = 'block'
+  console.log(textoRecebido)
+  return
+}
+
+function descriptografar() {
+  var textoRecebido = entradaTexto.value.toLowerCase()
+  textoCripto = textoRecebido
+    .replace(/ai/g, 'a')
+    .replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u')
+  var img = document.querySelector('.img-container')
+  var copy = document.querySelector('.button-copy')
+  var texto = document.querySelector('.container-texto')
+  document.querySelector('.container-texto').innerHTML = textoCripto
+  img.style.visibility = 'hidden'
+  copy.style.visibility = 'inherit'
+  texto.style.display = 'block'
+  console.log(textoRecebido)
+  return
+}
+
+function copiar() {
+  var copy = navigator.clipboard.writeText(textoCripto)
+  clear()
+}
+
+function clear() {
+  var img = document.querySelector('.img-container')
+  var copy = document.querySelector('.button-copy')
+  var texto = document.querySelector('.container-texto')
+  document.querySelector('.container-texto').innerHTML = textoCripto
+  img.style.visibility = 'visible'
+  copy.style.visibility = 'hidden'
+  texto.style.display = 'none'
+  document.querySelector('.container-texto').innerHTML = ''
+}
